4f1545aaf06604bcbe462a53a1291cfd11f4b7ed
Simon Fletcher <simon@wakecodesleep.com>
2016-05-31 14:15:22 -0700

    Small refactor.

--- src/index.js
+++ src/index.js
@@ -1,42 +1,86 @@
 export default function createInterceptorMiddleware() {
   let actions = [];
+  let promises = [];
+  let active = true;
   let resolved = false;
 
-  function resolve() {
+  function trackAction(exec) {
+    if (typeof exec.then === 'function') {
+      const untrack = () => {
+        const index = promises.indexOf(exec);
+
+        if (index > -1) {
+          promises.splice(index, 1);
+        }
+      };
+
+      promises.push(exec);
+      exec.then(untrack, untrack);
+    }
+
+    return exec;
+  }
+
+  function execAction({ resolve: _resolve, reject, next, action }) {
+    const exec = next(action);
+
+    if (typeof exec.then === 'function') {
+      return trackAction(exec).then(_resolve, reject);
+    }
+
+    return Promise.resolve(exec);
+  }
+
+  function handleActions(followChain = true) {
+    if (!actions.length) {
+      active = false;
+      return Promise.all(promises);
+    }
+
+    actions.map(execAction);
+
+    let promise = Promise.all(promises);
+
+    actions = [];
+    promises = [];
+
+    if (followChain) {
+      promise = promise.then(handleActions, handleActions);
+    } else {
+      active = false;
+    }
+
+    return promise;
+  }
+
+  function resolve({ followChain = true } = {}) {
     if (resolved) {
       return Promise.reject(new Error('the interceptor was already resolved'));
     }
 
     resolved = true;
 
-    if (!actions.length) {
-      return Promise.resolve();
-    }
-
-    const promise = Promise.all(actions.map(({ resolve: _resolve, reject, next, action }) => {
-      const exec = next(action);
-
-      if (typeof exec.then === 'function') {
-        exec.then(_resolve, reject);
-        return exec;
-      }
-
-      return Promise.resolve(exec);
-    }));
-
-    actions = [];
-
-    return promise;
+    return handleActions(followChain);
   }
 
   function reset() {
     actions = [];
+    promises = [];
+    active = true;
     resolved = false;
   }
 
   const middleware = () => next => action => {
-    if (resolved || (action.type && action.type.substr(0, 2) === '@@')) { // allow internal actions
+    if (action.type && action.type.substr(0, 2) === '@@') { // allow internal actions
       return next(action);
+    }
+
+    if (resolved) {
+      if (!active) {
+        return next(action);
+      }
+
+      return trackAction(next(action));
     }
 
     return new Promise((_resolve, reject) => {
